eaa7d0c60efb5188a4444ea601cd1981a56205d6
r1hamzar29-blip <r1hamzar29@gmail.com>
2025-12-09 12:30:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+(function () {
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get("id");
+
+    if (id) {
+        window.location.replace(
+            "https://example.com/?id=" + encodeURIComponent(id)
+        );
+    }
+})();
